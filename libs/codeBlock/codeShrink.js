ad36e5755f4893816d1b7925551222ce5c006356
DFameMaster <624634383@qq.com>
2026-04-07 22:26:19 +0800

    Site updated: 2026-04-07 22:26:14

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -2,9 +2,11 @@
 
 $(function () {
   var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
-
   $('.code .code-area').prepend($code_expand);
   $('.code .code-area').on('click', '.code-expand', function () {
+    // 向上查找tr元素
+    var trElement = $(this).closest('tr');
+    console.log("找到的tr元素:", trElement);
     if ($(this).parent().hasClass('code-closed')) {
       $(this).siblings('pre').find('code').show();
       $(this).parent().removeClass('code-closed');
