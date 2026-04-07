71dc1bc5b85e5794d048757fe8c463cde8731887
DFameMaster <624634383@qq.com>
2026-04-07 21:57:49 +0800

    Site updated: 2026-04-07 21:57:42

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -3,8 +3,8 @@
 $(function () {
   var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
 
-  $('.code-area').prepend($code_expand);
-  $('.code-expand').on('click', function () {
+  $('.code-area').eq(1).prepend($code_expand);
+  $('.code-area').eq(1).on('click', '.code-expand', function () {
     if ($(this).parent().hasClass('code-closed')) {
       $(this).siblings('pre').find('code').show();
       $(this).parent().removeClass('code-closed');
