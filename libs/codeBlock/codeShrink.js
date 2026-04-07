16f9ac7d71a562d71928d54e0a4bc2ec338f1780
DFameMaster <624634383@qq.com>
2026-04-07 22:57:30 +0800

    Site updated: 2026-04-07 22:57:26

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -4,13 +4,15 @@ $(function () {
   var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
   $('.code .code-area').prepend($code_expand);
   $('.code .code-area').on('click', '.code-expand', function () {
+    // 获取当前code-area元素
+    var codeAreaElement = $(this).parent();
     // 向上查找tr元素
     var trElement = $(this).closest('tr');
-    if (trElement.hasClass('code-closed')) {
-      // $(this).siblings('pre').find('code').show();
+    if (codeAreaElement.hasClass('code-closed')) {
+      codeAreaElement.removeClass('code-closed');
       trElement.removeClass('code-closed');
     } else {
-      // $(this).siblings('pre').find('code').hide();
+      codeAreaElement.addClass('code-closed');
       trElement.addClass('code-closed');
     }
   });
