64c7a9c78b760122338e864625a27ba3326f346b
DFameMaster <624634383@qq.com>
2026-04-13 14:55:34 +0800

    Site updated: 2026-04-13 14:55:10

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,16 +1,1 @@
-// 代码块收缩
-
-$(function () {
-  var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
-
-  $('.code .code-area').prepend($code_expand);
-  $('.code .code-area').on('click', '.code-expand', function () {
-    if ($(this).parent().hasClass('code-closed')) {
-      $(this).parent().parent().siblings('.gutter').show();
-      $(this).parent().removeClass('code-closed');
-    } else {
-      $(this).parent().parent().siblings('.gutter').hide();
-      $(this).parent().addClass('code-closed');
-    }
-  });
-});
+$(function(){var e=$('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');$(".code .code-area").prepend(e),$(".code .code-area").on("click",".code-expand",function(){$(this).parent().hasClass("code-closed")?($(this).parent().parent().siblings(".gutter").show(),$(this).parent().removeClass("code-closed")):($(this).parent().parent().siblings(".gutter").hide(),$(this).parent().addClass("code-closed"))})});
